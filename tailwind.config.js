155c0042aa301bc6e921626fa4f280f3f17e4dd5
LightLord <stevensah88@gmail.com>
2024-01-21 13:05:59 -0800

    deps: vite upgrade (vitawind old ver), feat: styling, content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    colors: {
+      text: "#e3ebe8",
+      background: "#0e1412",
+      primary: "#b4c9c3",
+      secondary: "#41535a",
+      accent: "#8293a6",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      dropShadow: {
+        lg: "1px 3px 40px #ffffff",
+      },
+    },
   },
   plugins: [],
-}
+};
